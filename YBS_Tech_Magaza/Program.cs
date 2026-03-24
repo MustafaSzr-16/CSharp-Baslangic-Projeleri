fdaa831c384e04df807588622ebd4155ee4ba4ff
MustafaSzr-16 <162330853+MustafaSzr-16@users.noreply.github.com>
2026-03-24 18:56:38 +0300

    YBS_Tech_Kasa

---
+++ YBS_Tech_Magaza/Program.cs
@@ -0,0 +1,25 @@
+﻿string[] urunler = {"bilgisayar","telefon","saat","televizyon"};
+int[] fiyatlar = {30000,20000,10000,5000};
+
+int toplamSepet = 0;
+string devamMi = "E";
+
+while(devamMi == "E" || devamMi == "e")
+{
+    for(int i = 0; i<urunler.Length; i++)
+    {
+        Console.WriteLine($"{i+1} - {urunler[i]} : {fiyatlar[i]} tl");
+    }
+
+    Console.WriteLine("Lütfen almak istediğiniz ürünün numarasını giriniz: ");
+    int secim = Convert.ToInt32(Console.ReadLine());
+    toplamSepet = toplamSepet + fiyatlar[secim-1];
+
+    Console.WriteLine($"Ürün sepete eklendi! Mevcut tutar: {toplamSepet} tl ");
+
+    Console.WriteLine("başka bir arzunuz varmı? (e/h)");
+    devamMi = Console.ReadLine();
+}
+
+Console.WriteLine("Bizi tercih ettiğiniz için teşekkürler.");
+Console.WriteLine($"ÖDENECEK TOPLAM TUTAR: {toplamSepet}  TL");
