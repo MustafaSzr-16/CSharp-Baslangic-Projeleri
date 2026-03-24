7aa81c72a5431149d93e31432c881fe2fe7019dd
MustafaSzr-16 <162330853+MustafaSzr-16@users.noreply.github.com>
2026-03-24 19:21:55 +0300

    Sayi_TahminOyunu

---
+++ Sayi_TahminOyunu/Program.cs
@@ -0,0 +1,29 @@
+﻿
+
+Random rnd = new Random();
+int tutulanSayi = rnd.Next(1,101);  // 1 dahil 101 dahil değildir
+
+int tahmin = 0;
+int denemeSayisi = 0;
+
+Console.WriteLine("Aklımdan 1 ile 100 arasında bir sayı tuttum. hadi bul.");
+
+while(tahmin != tutulanSayi)
+{
+    Console.WriteLine("Tahmininiz nedir: ");
+    tahmin = Convert.ToInt32(Console.ReadLine());
+    denemeSayisi++;
+
+    if (tahmin < tutulanSayi)
+    {
+        Console.WriteLine("daha büyük bir sayı söyle.");
+    }
+    else if (tahmin > tutulanSayi)
+    {
+        Console.WriteLine("daha küçük bir sayı söyle.");
+    }
+    else
+    {
+        Console.WriteLine($"Tebrikler! {denemeSayisi}. denemede buldunuz.");
+    }
+}
